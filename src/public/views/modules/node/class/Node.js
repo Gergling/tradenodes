c5a8f2f2dc9babcb7f7792f9379d538c2e3d1f53
gergling <gregmichaeldavies@gmail.com>
2014-12-29 14:56:29 +0000

    Now loops all types of messages.

--- src/public/views/modules/node/class/Node.js
+++ src/public/views/modules/node/class/Node.js
@@ -5,12 +5,24 @@ angular.module("node").factory("node.class.Node", [
     function (Production) {
         "use strict";
 
+        var AnalysisMessage = function (name, label, colour) {
+            var messages = [ ];
+            this.name = function () {return name; };
+            this.label = function () {return label; };
+            this.colour = function () {return colour; };
+            this.messages = function (value) {
+                if (value) {messages = value; };
+                return messages;
+            };
+        };
+
         return function (label) {
             var productions = [ ],
                 consumptions = [ ],
                 imports = [ ],
                 exports = [ ],
-                storage = [ ];
+                storage = [ ],
+                analysis;
 
             this.label = function () {return label; };
             this.productions = function (production) {
@@ -26,66 +38,79 @@ angular.module("node").factory("node.class.Node", [
 
             this.analysis = function () {
                 // Get total consumptions
-                var analysis = {
+                if (!analysis) {
+                    var messages = {
+                            anomalies: [ ],
+                            warning: [ ],
+                            waste: [ ]
+                        },
+                        surplus = { },
+                        setSurplus = function (order, add) {
+                            var name = order.product().name(),
+                                quantity = order.quantity();
+
+                            if (!surplus[name]) {surplus[name] = 0; }
+                            if (add) {
+                                surplus[name] += quantity;
+                            } else {
+                                surplus[name] -= quantity;
+                            }
+                        },
+                        addSurplus = function (order) {setSurplus(order, true); };
+
+                    analysis = {
                         productions: productions.length,
                         consumptions: consumptions.length,
                         imports: imports.length,
                         exports: exports.length,
                         storage: storage.length,
                         show: false,
-                        products: { },
-                        anomalies: [ ],
-                        warning: [ ],
-                        waste: [ ]
-                    },
-                    surplus = { },
-                    setSurplus = function (order, add) {
-                        var name = order.product().name(),
-                            quantity = order.quantity();
+                        messages: [
+                            new AnalysisMessage("warning", "Warnings", "#f55"),
+                            new AnalysisMessage("waste", "Waste", "orange"),
+                            new AnalysisMessage("anomalies", "Anomalies", "lightblue")
+                        ]
+                    };
 
-                        if (!surplus[name]) {surplus[name] = 0; }
-                        if (add) {
-                            surplus[name] += quantity;
-                        } else {
-                            surplus[name] -= quantity;
+                    // Internal
+                    productions.forEach(function (production) {
+                        addSurplus(production.product());
+                        if (production.labour().length === 0) {
+                            messages.anomalies.push([
+                                "No labour costs for production: ",
+                                production.product().product().name()
+                            ].join(""));
                         }
-                    },
-                    addSurplus = function (order) {setSurplus(order, true); };
+                    });
+                    consumptions.forEach(setSurplus);
 
-                // Internal
-                productions.forEach(function (production) {
-                    addSurplus(production.product());
-                    if (production.labour().length === 0) {
-                        analysis.anomalies.push([
-                            "No labour costs for production: ",
-                            production.product().product().name()
-                        ].join(""));
-                    }
-                });
-                consumptions.forEach(setSurplus);
+                    // External
+                    imports.forEach(addSurplus);
+                    exports.forEach(function (exp) {
+                        setSurplus(exp.order());
+                    });
 
-                // External
-                imports.forEach(addSurplus);
-                exports.forEach(function (exp) {
-                    setSurplus(exp.order());
-                });
+                    // Create analyses object and return.
+                    angular.forEach(surplus, function (quantity, name) {
+                        if (quantity < 0) {
+                            messages.warning.push("Immediate deficit: " + name + " (" + quantity + "/time unit)");
+                            analysis.show = true;
+                        }
+                        if (quantity > 0) {
+                            messages.waste.push(name + " (" + quantity + "/time unit)");
+                            analysis.show = true;
+                        }
+                    });
+                    // Total imports + productions - consumptions
+                    // = surplus
+                    // if surplus -ve, is deficit, number of years before starvation
+                    // if surplus +ve, number of years before storage space filled
+                    // if storage space 0, +ve surplus is all waste
 
-                // Create analyses object and return.
-                angular.forEach(surplus, function (quantity, name) {
-                    if (quantity < 0) {
-                        analysis.warning.push("Immediate deficit: " + name + " (" + quantity + "/time unit)");
-                        analysis.show = true;
-                    }
-                    if (quantity > 0) {
-                        analysis.waste.push(name + " (" + quantity + "/time unit)");
-                        analysis.show = true;
-                    }
-                });
-                // Total imports + productions - consumptions
-                // = surplus
-                // if surplus -ve, is deficit, number of years before starvation
-                // if surplus +ve, number of years before storage space filled
-                // if storage space 0, +ve surplus is all waste
+                    analysis.messages.forEach(function (message) {
+                        message.messages(messages[message.name()]);
+                    });
+                }
 
                 return analysis;
             };

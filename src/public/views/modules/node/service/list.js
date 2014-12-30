120836d3b608ddeb0dbf44bc1d2592c94465405f
gergling <gregmichaeldavies@gmail.com>
2014-12-30 10:56:22 +0000

    Economic refinement to handle new warning tests.

--- src/public/views/modules/node/service/list.js
+++ src/public/views/modules/node/service/list.js
@@ -19,14 +19,16 @@ angular.module("node").service("node.service.list", [
             buyer.consumptions(new Order(products.food, 3));
             list.push(buyer);
 
-            supplier.productions(new Production(new Order(products.food, 1)));
-            supplier.exports(new Export(new Order(products.food, 1), buyer));
+            supplier.productions(new Production(new Order(products.food, 4)));
+            supplier.consumptions(new Order(products.food, 3));
+            supplier.exports(new Export(new Order(products.food, 2), buyer));
             list.push(supplier);
 
             list.push(new Node("Dead World"));
 
             list.push(function () {
                 var node = new Node("Iron World");
+                node.consumptions(new Order(products.food, 2));
                 node.productions(function () {
                     var production = new Production(new Order(products.iron, 10));
                     // Labour costs for Iron.
